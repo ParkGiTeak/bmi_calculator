798fd39757b0cf0f51535ed43a449d3d283adc86
ParkGiTeak <rl980901@naver.com>
2025-07-01 14:47:12 +0900

    feat: MainScreen Widget에서 키와 몸무게를 받는 TextField의 Controller를 만들어서 입력된 text를 페이지 이동할때 넘겨주도록 처리

--- lib/main/main_screen.dart
+++ lib/main/main_screen.dart
@@ -10,6 +10,15 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   final _formKey = GlobalKey<FormState>();
+  final _heightController = TextEditingController();
+  final _weightController = TextEditingController();
+
+  @override
+  void dispose() {
+    _heightController.dispose();
+    _weightController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +34,7 @@ class _MainScreenState extends State<MainScreen> {
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               TextFormField(
+                controller: _heightController,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   hintText: '키',
@@ -40,6 +50,7 @@ class _MainScreenState extends State<MainScreen> {
               ),
               SizedBox(height: 8),
               TextFormField(
+                controller: _weightController,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   hintText: '몸무게',
@@ -56,14 +67,16 @@ class _MainScreenState extends State<MainScreen> {
               SizedBox(height: 8),
               ElevatedButton(
                 onPressed: () {
-                  if (_formKey.currentState?.validate() ?? false) {
+                  if (_formKey.currentState?.validate() == false) {
                     return;
                   }
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) =>
-                          ResultScreen(height: 180, weight: 77.5),
+                      builder: (context) => ResultScreen(
+                        height: double.parse(_heightController.text),
+                        weight: double.parse(_weightController.text),
+                      ),
                     ),
                   );
                 },
