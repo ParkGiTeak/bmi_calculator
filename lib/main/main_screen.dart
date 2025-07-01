93dd81ddcb8432e5c395f1a5bcab1bf4d5e8810a
ParkGiTeak <rl980901@naver.com>
2025-07-01 15:13:32 +0900

    feat: sharedPrefernce를 활용하여 결과 버튼을 누르면 키와 몸무게 TextEditingController의 text값을 저장하고 initState에서는 값을 읽어오도록 기능 추가

--- lib/main/main_screen.dart
+++ lib/main/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:bmi_calculator/result/result_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -14,10 +15,34 @@ class _MainScreenState extends State<MainScreen> {
   final _weightController = TextEditingController();
 
   @override
+  void initState() {
+    super.initState();
+    load();
+  }
+
+  @override
   void dispose() {
     _heightController.dispose();
     _weightController.dispose();
     super.dispose();
+  }
+
+  Future save() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setDouble('height', double.parse(_heightController.text));
+    await prefs.setDouble('weight', double.parse(_weightController.text));
+  }
+
+  Future load() async {
+    final prefs = await SharedPreferences.getInstance();
+    final double? height = prefs.getDouble('height');
+    final double? weight = prefs.getDouble('weight');
+
+    if (height != null && weight != null) {
+      _heightController.text = height.toString();
+      _weightController.text = weight.toString();
+    }
   }
 
   @override
@@ -70,6 +95,7 @@ class _MainScreenState extends State<MainScreen> {
                   if (_formKey.currentState?.validate() == false) {
                     return;
                   }
+                  save();
                   Navigator.push(
                     context,
                     MaterialPageRoute(
