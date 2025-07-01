f8f214fc61114e8e71245b892d5ac0b0039e1e81
ParkGiTeak <rl980901@naver.com>
2025-07-01 14:41:57 +0900

    feat: ResultScreen Widget bmi 결과 Text Widget, Icon Widget bmi 지수에 따른 분기처리 사용하는 메서드 추가

--- lib/result/result_screen.dart
+++ lib/result/result_screen.dart
@@ -10,8 +10,48 @@ class ResultScreen extends StatelessWidget {
     required this.weight,
   });
 
+  String _calcBmi(double bmi) {
+    String result = '저체중';
+
+    if (bmi >= 35) {
+      result = '고도 비만';
+    } else if (bmi >= 30) {
+      result = '2단계 비만';
+    } else if (bmi >= 25) {
+      result = '1단계 비만';
+    } else if (bmi >= 23) {
+      result = '과체중';
+    } else if (bmi >= 18.5) {
+      result = '정상';
+    }
+    return result;
+  }
+
+  Widget _buildIcon(double bmi) {
+    Icon icon = Icon(
+      Icons.sentiment_dissatisfied,
+      color: Colors.green,
+      size: 100,
+    );
+    if (bmi >= 23) {
+      icon = Icon(
+        Icons.sentiment_very_dissatisfied,
+        color: Colors.green,
+        size: 100,
+      );
+    } else if (bmi >= 18.5) {
+      icon = Icon(
+        Icons.sentiment_satisfied,
+        color: Colors.green,
+        size: 100,
+      );
+    }
+    return icon;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final bmi = weight / ((height / 100.0) * (height / 100.0));
     return Scaffold(
       appBar: AppBar(
         title: Text('결과'),
@@ -20,14 +60,10 @@ class ResultScreen extends StatelessWidget {
         child: Column(
           children: [
             Text(
-              '정상',
+              _calcBmi(bmi),
               style: TextStyle(fontSize: 36),
             ),
-            Icon(
-              Icons.sentiment_satisfied,
-              color: Colors.green,
-              size: 100,
-            ),
+            _buildIcon(bmi)
           ],
         ),
       ),
